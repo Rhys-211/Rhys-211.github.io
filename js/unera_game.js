b69aecaaaf8ab74f49daec6ee53a3c1be3b42c5c
Mingrui Lu <2815478278qq@gmail.com>
2017-07-25 16:01:50 +0800

    修改了加载机制

--- js/unera_game.js
+++ js/unera_game.js
@@ -1,3 +1,15 @@
 window.onload = function(){
-    main();
+    /*const startbgm = document.getElementById('startbgm');
+    startbgm.addEventListener('canplaythrough',function(){
+        startbgm.play();
+        alert('')
+    })*/
+    const startGame = document.getElementById('startGame');
+    var startbgm = document.createElement("audio");
+    if (startbgm != null && startbgm.canPlayType && startbgm.canPlayType("audio/mpeg")){
+        startbgm.src = "audio/startbgm.mp3";
+        startbgm.play();
+        startGame.innerText = 'Play';
+        startGame.setAttribute('onclick','alert("开发中")');
+    }
 }
