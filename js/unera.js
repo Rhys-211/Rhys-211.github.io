f7ed77c297ba74631c3d94f01f789579d0a98eb3
Mingrui Lu <2815478278qq@gmail.com>
2017-07-25 15:24:16 +0800

    完善了游戏页

---
+++ js/unera.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    main();
+    if(prompt('请输入预览密码') == 'パスワード'){
+        alert("密码正确");
+        document.getElementById('gamebox').setAttribute('src','unera_game.html')
+    }
+    else
+        alert("密码错误")
+}
