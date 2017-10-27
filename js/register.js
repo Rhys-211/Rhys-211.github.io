99650b42cb8bfc6dfb735020dff9c5d95aa64b3a
Mingrui Lu <rhyther211@gmail.com>
2017-10-27 20:11:42 +0800

    Added&Fixed
    1.为登陆与注册增加了回车热键；
    2.修复了readme.md中一处语序错乱问题。

--- js/register.js
+++ js/register.js
@@ -15,6 +15,64 @@ window.onload = function () {
     const pwd2_info = document.getElementById('pwd2_info');
     const username_info = document.getElementById('username_info');
     const userEmail_info = document.getElementById('userEmail_info');
+    function register() {
+        register_btn.innerText = '注册中......';
+        register_btn.disabled = 'disabled';
+        if (acntNmb_usable == pwd_usable && pwd_usable == pwd2_usable && pwd2_usable == username_usable && username_usable == userEmail_usable && userEmail_usable == true) {
+            Bmob.initialize("c4c8b7af88a34d5d587b8d15506b1882", "4298aaed28dfc11c8a492d1828d93539");
+            var User = Bmob.Object.extend("sfUser");
+            var query = new Bmob.Query(User);
+            query.find({
+                success: function (results) {
+                    //开始查询用户名
+                    for (var i = 0; i < results.length; i++) {
+                        object = results[i];
+                        //查询到用户名
+                        if (object.get('accountNumber') == acntNmb.value) {
+                            alert('注册失败\n返回错误信息：账号与其他用户重复！');
+                            acntNmb_info.innerText = '账号重复';
+                            acntNmb_info.style.color = '#FF0000';
+                            acntNmb_usable = false;
+                            register_btn.innerText = '注册';
+                            register_btn.disabled = false;
+                            break;
+                            //未查询到用户名
+                        } else if (i == results.length - 1) {
+                            var user = new User();
+                            user.set("accountNumber", acntNmb.value);
+                            user.set("password", pwd.value);
+                            user.set("username", username.value);
+                            user.set("email", userEmail.value);
+                            user.set("info", '');
+                            user.set("avatarUrl", '/images/userAvatar.png');
+                            user.save(null, {
+                                success: function (user) {
+                                    alert('注册成功！');
+                                    register_btn.innerText = '注册成功';
+                                    window.open('/user/signIn.html', '_self')
+                                },
+                                error: function (user, error) {
+                                    alert('注册失败\n返回错误码：' + error.code + '\n返回错误信息：' + error.message);
+                                    register_btn.innerText = '注册';
+                                    register_btn.disabled = false;
+                                }
+                            });
+                        }
+                    }
+                },
+                error: function (error) {
+                    alert('注册失败\n返回错误码：' + error.code + '\n返回错误信息：' + error.message);
+                    register_btn.innerText = '注册';
+                    register_btn.disabled = false;
+                }
+            });
+        }
+        else {
+            alert('注册失败\n返回错误信息：请检查上述诸项是否均符合要求。');
+            register_btn.innerText = '注册';
+            register_btn.disabled = false;
+        }
+    }
     acntNmb.addEventListener('change', function () {
         if (acntNmb.value == '') {
             acntNmb_info.innerText = '请输入账号';
@@ -120,62 +178,9 @@ window.onload = function () {
             userEmail_usable = true;
         }
     });
-    document.getElementById('register_btn').onclick = function () {
-        register_btn.innerText = '注册中......';
-        register_btn.disabled = 'disabled';
-        if (acntNmb_usable == pwd_usable && pwd_usable == pwd2_usable && pwd2_usable == username_usable && username_usable == userEmail_usable && userEmail_usable == true) {
-            Bmob.initialize("c4c8b7af88a34d5d587b8d15506b1882", "4298aaed28dfc11c8a492d1828d93539");
-            var User = Bmob.Object.extend("sfUser");
-            var query = new Bmob.Query(User);
-            query.find({
-                success: function (results) {
-                    //开始查询用户名
-                    for (var i = 0; i < results.length; i++) {
-                        object = results[i];
-                        //查询到用户名
-                        if (object.get('accountNumber') == acntNmb.value) {
-                            alert('注册失败\n返回错误信息：账号与其他用户重复！');
-                            acntNmb_info.innerText = '账号重复';
-                            acntNmb_info.style.color = '#FF0000';
-                            acntNmb_usable = false;
-                            register_btn.innerText = '注册';
-                            register_btn.disabled = false;
-                            break;
-                            //未查询到用户名
-                        } else if (i == results.length - 1) {
-                            var user = new User();
-                            user.set("accountNumber", acntNmb.value);
-                            user.set("password", pwd.value);
-                            user.set("username", username.value);
-                            user.set("email", userEmail.value);
-                            user.set("info", '');
-                            user.set("avatarUrl", '/images/userAvatar.png');
-                            user.save(null, {
-                                success: function (user) {
-                                    alert('注册成功！');
-                                    register_btn.innerText = '注册成功';
-                                    window.open('/user/signIn.html','_self')
-                                },
-                                error: function (user, error) {
-                                    alert('注册失败\n返回错误码：' + error.code + '\n返回错误信息：' + error.message);
-                                    register_btn.innerText = '注册';
-                                    register_btn.disabled = false;
-                                }
-                            });
-                        }
-                    }
-                },
-                error: function (error) {
-                    alert('注册失败\n返回错误码：' + error.code + '\n返回错误信息：' + error.message);
-                    register_btn.innerText = '注册';
-                    register_btn.disabled = false;
-                }
-            });
-        }
-        else {
-            alert('注册失败\n返回错误信息：请检查上述诸项是否均符合要求。');
-            register_btn.innerText = '注册';
-            register_btn.disabled = false;
-        }
-    };
+    document.getElementById('register_btn').addEventListener('click', register)
+    document.addEventListener('keydown', function (event) {
+        if (event.key == 'Enter')
+            register()
+    })
 }
--- js/signIn.js
+++ js/signIn.js
@@ -1,50 +1,28 @@
-window.onload = function(){
+window.onload = function () {
     main();
     const acntNmb = document.getElementById('acntNmb_input');
     const pwd = document.getElementById('pwd_input');
     const acntNmb_info = document.getElementById('acntNmb_info');
     const pwd_info = document.getElementById('pwd_info');
-    acntNmb.addEventListener('change',function(){
-        if(acntNmb.value == ''){
-            acntNmb_info.innerText = '请输入账号';
-            acntNmb_info.style.color = 'orange';
-            acntNmb_usable = false;
-        }else{
-            acntNmb_info.innerText = '已输入账号';
-            acntNmb_info.style.color = '#32CD32';
-            acntNmb_usable = true;
-        }
-    });
-    pwd.addEventListener('change',function(){
-        if(pwd.value == ''){
-            pwd_info.innerText = '请输入密码';
-            pwd_info.style.color = 'orange';
-            pwd_usable = false;
-        }else{
-            pwd_info.innerText = '已输入密码';
-            pwd_info.style.color = '#32CD32';
-            pwd_usable = true;
-        }
-    });
-    document.getElementById('signIn_btn').onclick = function(){
+    function signIn() {
         signIn_btn.innerText = '登录中......';
         signIn_btn.disabled = 'disabled';
-        if(acntNmb.value != '' && pwd.value != ''){
+        if (acntNmb.value != '' && pwd.value != '') {
             Bmob.initialize("c4c8b7af88a34d5d587b8d15506b1882", "4298aaed28dfc11c8a492d1828d93539");
             var User = Bmob.Object.extend("sfUser");
             var query = new Bmob.Query(User);
             query.find({
-                success: function(results) {
+                success: function (results) {
                     //开始查询用户名
                     for (var i = 0; i < results.length; i++) {
                         object = results[i];
                         //查询到用户名
-                        if (object.get('accountNumber') == acntNmb.value){
+                        if (object.get('accountNumber') == acntNmb.value) {
                             writeCookies(object);
-                            if(pwd.value == object.get('password')){
+                            if (pwd.value == object.get('password')) {
                                 signIn_btn.innerText = '已登录';
                                 alert('登录成功！！！');
-                                window.open('/index.html','_self')
+                                window.open('/index.html', '_self')
                                 break;
                             } else {
                                 pwd_info.innerText = '密码错误';
@@ -53,9 +31,9 @@ window.onload = function(){
                                 signIn_btn.innerHTML = '登&nbsp;&nbsp;&nbsp;&nbsp;录';
                                 signIn_btn.disabled = false;
                                 break;
-                            }  
-                        //未查询到用户名
-                        } else if (i == results.length - 1){
+                            }
+                            //未查询到用户名
+                        } else if (i == results.length - 1) {
                             acntNmb_info.innerText = '用户名不存在';
                             acntNmb_info.style.color = 'red';
                             acntNmb_usable = false;
@@ -64,7 +42,7 @@ window.onload = function(){
                         }
                     }
                 },
-                error: function(error) {
+                error: function (error) {
                     alert('登陆失败\n返回错误码：' + error.code + '\n返回错误信息：' + error.message);
                     signIn_btn.innerHTML = '登&nbsp;&nbsp;&nbsp;&nbsp;录';
                     signIn_btn.disabled = false;
@@ -76,4 +54,31 @@ window.onload = function(){
             signIn_btn.disabled = false;
         }
     };
+    acntNmb.addEventListener('change', function () {
+        if (acntNmb.value == '') {
+            acntNmb_info.innerText = '请输入账号';
+            acntNmb_info.style.color = 'orange';
+            acntNmb_usable = false;
+        } else {
+            acntNmb_info.innerText = '已输入账号';
+            acntNmb_info.style.color = '#32CD32';
+            acntNmb_usable = true;
+        }
+    });
+    pwd.addEventListener('change', function () {
+        if (pwd.value == '') {
+            pwd_info.innerText = '请输入密码';
+            pwd_info.style.color = 'orange';
+            pwd_usable = false;
+        } else {
+            pwd_info.innerText = '已输入密码';
+            pwd_info.style.color = '#32CD32';
+            pwd_usable = true;
+        }
+    });
+    document.getElementById('signIn_btn').addEventListener('click', signIn)
+    document.addEventListener('keydown', function (event) {
+        if (event.key == 'Enter')
+            signIn()
+    })
 }
