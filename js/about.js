d0fbaa949a148b8c4a372e1e51d9b49066668d1c
Mingrui Lu <rhyther211@gmail.com>
2017-10-28 19:07:00 +0800

    Added&Deleted&Improved
    1.为駴歍增加了两首BGM（随机出现），并调整了其位置；
    2.删除了一张多余图片；
    3.其他关于关于我们的细节调整。
    3.修改了readme.md的一处描述。

---
+++ js/about.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    main()
+    var musicOfHaiyang = document.querySelector('#musicOfHaiyang')
+    var srcOfHaiyang = '//music.163.com/outchain/player?type=2&id='
+    if(Math.random() < 0.5)
+        srcOfHaiyang += '33911781&auto=0&height=66'
+    else
+        srcOfHaiyang += '28828078&auto=0&height=66'
+        musicOfHaiyang.setAttribute('src',srcOfHaiyang)
+}
