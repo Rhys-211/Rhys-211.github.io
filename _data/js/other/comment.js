c1f265d0dd1ebe7a063eb662a943012d1ce90aca
Mingrui Lu <rhyther211@gmail.com>
2017-11-11 14:24:21 +0800

    评论区发送信息时附带系统信息

--- _data/js/other/comment.js
+++ _data/js/other/comment.js
@@ -15,10 +15,11 @@ window.onload = function () {
 
     commentQuery.find({
         success: function (results) {
-            //获取commentItem所需的主要信息（内容，用户ID，发送时间）
+            //获取commentItem所需的主要信息（内容，系统，用户ID，发送时间）
             for (let i = 0; i < results.length; i++) {
                 comments[i] = {};
                 comments[i].content = results[i].attributes.content;
+                comments[i].system = results[i].attributes.system;
                 comments[i].userId = results[i].attributes.userId;
                 comments[i].createdAt = results[i].createdAt;
             }
@@ -31,15 +32,27 @@ window.onload = function () {
                 let commentItem_username = document.createElement('p')
                 let commentItem_postingTime = document.createElement('p')
                 let commentItem_content = document.createElement('p')
+                let commentItem_system = document.createElement('img')
                 //对commentItem中的元素的的属性进行常规赋值操作（主要是一些CSS样式）
                 commentItem_link.setAttribute('target', '_blank')
+                commentItem_userAvatar.setAttribute('class', 'avatar')
                 commentItem_username.setAttribute('class', 'commentItem_username')
                 commentItem_postingTime.setAttribute('class', 'commentItem_postingTime')
+                commentItem_system.setAttribute('class', 'commentItem_system')
                 commentItem_content.setAttribute('class', 'commentItem_content')
-                //对commentItem中的元素的主要内容进行赋值操作（个人主页地址，发送时间，内容）
+                //对commentItem中的元素的主要内容进行赋值操作（个人主页地址，发送时间，内容,系统）
                 commentItem_link.setAttribute('href', '/user/profile.html?id=' + comments[i].userId)
                 commentItem_postingTime.innerText = comments[i].createdAt
                 commentItem_content.innerText = comments[i].content
+                if(comments[i].system[0] == 'Win8-10')
+                    commentItem_system.setAttribute('src','/_data/images/comment/Win8-10.jpg')
+                if(comments[i].system[0] == 'Windows')
+                    commentItem_system.setAttribute('src','/_data/images/comment/Windows.jpg')
+                else if(comments[i].system[0] == 'Apple')
+                    commentItem_system.setAttribute('src','/_data/images/comment/Apple.jpg')
+                else if(comments[i].system[0] == 'Android')
+                    commentItem_system.setAttribute('src','/_data/images/comment/Android.jpg')
+                commentItem_system.setAttribute('title',comments[i].system[1])
                 //对commentItem中的元素的剩余用户内容进行赋值操作（用户名，头像）
                 userQuery.get(comments[i].userId, {
                     success: function (user) {
@@ -59,6 +72,7 @@ window.onload = function () {
                 commentItem.appendChild(commentItem_link);
                 commentItem.appendChild(commentItem_username);
                 commentItem.appendChild(commentItem_postingTime);
+                commentItem.appendChild(commentItem_system);
                 commentItem.appendChild(commentItem_content);
                 commentItems.appendChild(commentItem);
             }
@@ -75,43 +89,66 @@ window.onload = function () {
                 comment_btn.disabled = 'disabled';
                 comment_btn.innerText = '发布中......'
                 //获取系统版本
-                /*
                 var ua = navigator.userAgent
-                var system
-                if (ua.indexOf('Windows NT 5.0') > -1 || ua.indexOf("Windows 2000") > -1)
-                    system = 'Windows 2000'
-                else if (ua.indexOf('Windows NT 5.1') > -1 || ua.indexOf("Windows XP") > -1)
-                    system = 'Windows XP';
-                else if (ua.indexOf('Windows NT 5.2') > -1 || ua.indexOf("Windows 2003") > -1)
-                    system = 'Windows 2003'
-                else if (ua.indexOf('Windows NT 6.0') > -1 || ua.indexOf("Windows Vista") > -1)
-                    system = 'Windows Vista'
-                else if (ua.indexOf('Windows NT 6.1') > -1 || ua.indexOf("Windows 7") > -1)
-                    system = 'Windows 7'
-                else if (ua.indexOf('Windows NT 6.2') > -1 || ua.indexOf("Windows 8") > -1)
-                    system = 'Window 8'
-                else if (ua.indexOf('Windows NT 6.3') > -1 || ua.indexOf("Windows 8.1") > -1)
-                    system = 'Windows 8.1'
-                else if (ua.indexOf('Windows NT 10.0') > -1 || ua.indexOf("Windows 10") > -1)
-                    system = 'Windows 10'
-                else if (ua.indexOf('Android') > -1)
-                    system = ua.split(';')[1]
-                else if (ua.indexOf('iPhone') > -1)
-                    system = 'IOS ' + navigator.userAgent.match(/CPU iPhone OS (.*?) like Mac OS X/)[1].replace(/_/g,'.')
-                else if (ua.indexOf('Linux') > -1)
-                    system = 'Linux'
-                else
-                    system = "未知系统"
+                var system = new Array(2)
+                if (ua.indexOf('Windows NT 5.0') > -1 || ua.indexOf("Windows 2000") > -1) {
+                    system[0] = 'Windows'
+                    system[1] = 'Windows 2000'
+                }
+                else if (ua.indexOf('Windows NT 5.1') > -1 || ua.indexOf("Windows XP") > -1) {
+                    system[0] = 'Windows'
+                    system[1] = 'Windows XP';
+                }
+                else if (ua.indexOf('Windows NT 5.2') > -1 || ua.indexOf("Windows 2003") > -1) {
+                    system[0] = 'Windows'
+                    system[1] = 'Windows 2003'
+                }
+                else if (ua.indexOf('Windows NT 6.0') > -1 || ua.indexOf("Windows Vista") > -1) {
+                    system[0] = 'Windows'
+                    system[1] = 'Windows Vista'
+                }
+                else if (ua.indexOf('Windows NT 6.1') > -1 || ua.indexOf("Windows 7") > -1) {
+                    system[0] = 'Windows'
+                    system[1] = 'Windows 7'
+                }
+                else if (ua.indexOf('Windows NT 6.2') > -1 || ua.indexOf("Windows 8") > -1) {
+                    system[0] = 'Win8-10'
+                    system[1] = 'Window 8'
+                }
+                else if (ua.indexOf('Windows NT 6.3') > -1 || ua.indexOf("Windows 8.1") > -1) {
+                    system[0] = 'Win8-10'
+                    system[1] = 'Windows 8.1'
+                }
+                else if (ua.indexOf('Windows NT 10.0') > -1 || ua.indexOf("Windows 10") > -1) {
+                    system[0] = 'Win8-10'
+                    system[1] = 'Windows 10'
+                }
+                else if (ua.indexOf('Android') > -1) {
+                    system[0] = 'Android'
+                    system[1] = ua.split(';')[1]
+                }
+                else if (ua.indexOf('iPhone') > -1) {
+                    system[0] = 'Apple'
+                    system[1] = 'IOS ' + navigator.userAgent.match(/CPU iPhone OS (.*?) like Mac OS X/)[1].replace(/_/g, '.')
+                }
+                else if (ua.indexOf('Linux') > -1) {
+                    system[0] = 'Linux'
+                    system[1] = 'Linux'
+                }
+                else {
+                    system[0] = 'other'
+                    system[1] = "未知系统"
+                }
                 if (ua.indexOf("x64") > -1)
-                    system += ' 64位'
+                    system[1] += ' 64位'
                 else if (ua.indexOf("x32") > -1)
-                    system += ' 32位'
-                */
+                    system[1] += ' 32位'
+
                 commentQuery.find({
                     success: function (results) {
                         var comment = new Comment();
                         comment.set("userId", json.id);
-                        /*comment.set("systemVersion", system);*/
+                        comment.set("system", system);
                         comment.set("content", comment_input.value);
                         comment.save(null, {
                             success: function (comment) {
