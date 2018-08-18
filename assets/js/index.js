51f457fcb2e71ea7998dffe6122de21caa10a370
Rhys-211 <rhys.211sfs@qq.com>
2018-08-18 18:14:26 +0800

    为开始按钮增加了事件

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,6 @@
 document.getElementById("content").
 style.height = (window.innerHeight - 60) + 'px'
+
+document.querySelector('#start_btn').addEventListener('click',function(){
+    showSidebar()
+})
